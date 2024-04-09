f4c5ed933d5ad769ceaa0eeda6528d262d47a675
aresa.jyjang <jyjang@aresa.io>
2024-04-09 17:59:19 +0900

    update webp

--- src/components/bxPublishingDetail.tsx
+++ src/components/bxPublishingDetail.tsx
@@ -18,7 +18,7 @@ export default function BxPublishingDetail({ id }: { id: string }) {
         ></Image>
         <div className="absolute bottom-0 h-[20rem] w-full bg-gradient-to-b from-white/0 to-white/100"></div>
       </div>
-      <div className="mx-5 flex flex-col gap-12 text-left sm:mx-[7.5rem]">
+      <div className="mx-6 flex flex-col gap-12 text-left sm:mx-[7.5rem]">
         <div className="flex flex-col gap-1">
           <p className="text-sm text-navyBlue/70">{item?.category}</p>
           <h1 className="text-[2.625rem] font-bold leading-none text-navyBlue">
--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -62,7 +62,7 @@ export default function Modal({ children }: props) {
               </svg>
             </div>
           </div>
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+          <div className="flex min-h-full items-end justify-center p-0 text-center sm:items-center sm:p-0">
             <Transition.Child
               as={Fragment}
               enter="ease-out duration-300"
